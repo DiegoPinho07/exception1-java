3b346a7b740df4c9df63cd1fbfd7d97c4fde6396
Diego Pinho <diegoftpinho@gmail.com>
2024-02-02 06:00:19 -0300

    atualização com comentários

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -11,32 +11,36 @@ import model.entities.Reservation;
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
-		
+		 //chamando as classes scanner e simpledateformat ler dados e formatar datas
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
+         // aplicação do programa
 		System.out.print("Room number: ");
 		int number = sc.nextInt();
 		System.out.print("Check-in date (dd/MM/yyyy): ");
 		Date checkIn = sdf.parse(sc.next());
 		System.out.print("Check-out date (dd/MM/yyyy): ");
 		Date checkOut = sdf.parse(sc.next());
-
+           //comparação de datas usando o método after
 		if (!checkOut.after(checkIn)) {
 			System.out.println("Error in reservation: Check-out date must be after check-in date");
 		}
 		else {
+			//instanciando a classe criada reservation
 			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);
 
 			System.out.println();
 			System.out.println("Enter data to update the reservation:");
 			System.out.print("Check-in date (dd/MM/yyyy): ");
+			//método que pega os dados digitados e formata na forma de data desejada
 			checkIn = sdf.parse(sc.next());
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 
+			//criação de uma variável date para comparar dados atualizados da aplicação
 			Date now = new Date();
+			//nova verificação para confirmar se os dados foram digitados corretamente
 			if (checkIn.before(now) || checkOut.before(now)) {
 				System.out.println("Error in reservation: Reservation dates for update must be future dates");
 			}
@@ -44,7 +48,9 @@ public class Program {
 				System.out.println("Error in reservation: Check-out date must be after check-in date");
 			}
 			else {
+				//chamando o método updateDates para alterar o valor que está na nos argumentos checkIn e CheckOut
 				reservation.updateDates(checkIn, checkOut);
+				//estando tudo certo, o programa imprime o string da classe reservation
 				System.out.println("Reservation: " + reservation);
 			}
 		}
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -6,19 +6,19 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Reservation {
-	
+	//criando as variáveis da classe
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
-
+    //chamando o método simpledateformat como static para ser usado somente nessa classe
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
+   //criação dos construtores com argumentos
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
 	}
-
+    //chamando os getters e setters
 	public Integer getRoomNumber() {
 		return roomNumber;
 	}
@@ -34,17 +34,18 @@ public class Reservation {
 	public Date getCheckOut() {
 		return checkOut;
 	}
-
+    //criando um método para calcular a duração de um período. o mesmo precisa ser do tipo long pelo tamanho do dado 
 	public long duration() {
 		long diff = checkOut.getTime() - checkIn.getTime();
+		//método que converte uma unidade de tempo em outra unidade de tempo para fins de cálculo
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-
+    //método de atualização do tipo void pois não haverá retorno já que nessa solução as verificações são no programa principal
 	public void updateDates(Date checkIn, Date checkOut) {
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
 	}
-
+    //string para imprimir na tela
 	@Override
 	public String toString() {
 		return "Room "
